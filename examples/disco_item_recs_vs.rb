0660fda643a7da48ed8ab0d44d40ebc217b568f2
Andrew Kane <andrew@ankane.org>
2025-09-07 14:44:24 -0700

    Added example for vector sets [skip ci]

---
+++ examples/disco_item_recs_vs.rb
@@ -0,0 +1,15 @@
+require "disco"
+require "neighbor-redis"
+
+Neighbor::Redis.client = RedisClient.config.new_pool
+
+index = Neighbor::Redis::VectorSet.new("movies")
+index.drop if index.exists?
+
+data = Disco.load_movielens
+recommender = Disco::Recommender.new(factors: 20)
+recommender.fit(data)
+
+index.add_all(recommender.item_ids, recommender.item_factors)
+
+pp index.nearest("Star Wars (1977)").map { |v| v[:id] }
